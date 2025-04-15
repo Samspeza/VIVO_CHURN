37a7048446ec9464c636ec9b54c95fa32ddfd325
Samantha Vico <sanvic77@gmail.com>
2025-04-15 13:58:34 -0300

    #ajustar erros de linhas de interpretação

--- master.r
+++ master.r
@@ -4,7 +4,7 @@ library(ggplot2)
 library(cowplot)
 library(corrplot)
 
-
+#ajustar erros de linhas de interpretação
 #carregando dados para o Data Frame (dados)
 dados <-read.csv("VIVO_CHURN.csv", stringsAsFactors = T)
 #Visualização dos dados
